b1d6d1383768b2d55be996d4ef9dcd6508909a7d
TimBoys <786575400@qq.com>
2019-04-30 16:32:19 +0800

    “INIT

---
+++ precache-manifest.54056ca781ef0ab22e7855455aac3c9a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aeb66ed7f70b87f898c02e3bb7254e5",
+    "url": "./static/media/sun.2aeb66ed.png"
+  },
+  {
+    "revision": "ad96b09497c9621a870446c2775cdada",
+    "url": "./static/media/loginBgi.ad96b094.jpg"
+  },
+  {
+    "revision": "75cdf31a4a0da0ec634f0a085c11437c",
+    "url": "./static/media/avatar.75cdf31a.jpg"
+  },
+  {
+    "revision": "55129156959a02622c327a1e4a57eaf4",
+    "url": "./static/media/404.55129156.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2f51bd36cc36129ebbcf",
+    "url": "./static/js/main.5f36234b.chunk.js"
+  },
+  {
+    "revision": "7a09ba2ef07707d5e7fd",
+    "url": "./static/js/2.9eef4b20.chunk.js"
+  },
+  {
+    "revision": "2f51bd36cc36129ebbcf",
+    "url": "./static/css/main.29c9d6fd.chunk.css"
+  },
+  {
+    "revision": "7a09ba2ef07707d5e7fd",
+    "url": "./static/css/2.05f22009.chunk.css"
+  },
+  {
+    "revision": "d540838dd4216cd2528bda0c8727ce7a",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.779b8af8a4c24e9f20e2ee0e8206473a.js"
+  "./precache-manifest.54056ca781ef0ab22e7855455aac3c9a.js"
 );
 
 workbox.clientsClaim();
